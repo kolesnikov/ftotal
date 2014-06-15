2bfef8a0f75ce906d9ec26993639594a8d611d4c
Eugene Kolesnikov <eugene@kolesnikov.me>
2014-06-15 19:01:07 +0400

    Настройка базы

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=ftotal',
     'username' => 'dbuser',
-    'password' => '123',
+    'password' => 'qxmm4o',
     'charset' => 'utf8',
 ];
